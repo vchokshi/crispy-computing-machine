d10747b220b11cd08a2277e0c1d10dd49c28e255
Vihar Chokshi <vc@iot4.net>
2021-05-21 21:17:12 -0700

    Rando Comment

---
+++ roots/aws/us-east-1/kms.tf
@@ -0,0 +1,142 @@
+resource aws_kms_key packer {
+  is_enabled          = true
+  enable_key_rotation = true
+
+  description              = "KMS emcrypted key to encrypt AMI published"
+  deletion_window_in_days  = 30
+  key_usage                = "ENCRYPT_DECRYPT"
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+
+  policy = data.aws_iam_policy_document.packer_encryption_key_policy.json
+}
+
+resource aws_kms_alias packer {
+  name          = "alias/packer"
+  target_key_id = aws_kms_key.packer.key_id
+}
+
+######
+## KMS KEY POLICY
+######
+
+data "aws_iam_policy_document" "packer_encryption_key_policy" {
+
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      ]
+    }
+
+    actions = ["kms:*"]
+
+    resources = ["*"]
+  }
+
+  statement {
+
+    sid    = "Key Administration"
+    effect = "Allow"
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/vchokshi"
+      ]
+    }
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+  statement {
+    sid    = "Key Use (Encryption)"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/vchokshi",
+      ]
+    }
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+  statement {
+    sid    = "Key Use (Launch)"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "*"
+      ]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalOrgID"
+      values   = ["o-omgvxoevhj"]
+    }
+
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*",
+      "kms:ReEncryptFrom",
+      "kms:DescribeKey"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+  statement {
+    sid    = "Allow attachment of persistent resources"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "*"
+      ]
+    }
+    actions = [
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:RevokeGrant"
+    ]
+    resources = [
+      "*"
+    ]
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values   = ["true"]
+    }
+
+  }
+}
