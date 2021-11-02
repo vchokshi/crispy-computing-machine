1d2fe08a21fa33d299781957089bded42974652d
Vihar Chokshi <vc@iot4.net>
2021-11-01 19:17:55 -0700

    [DENVER U] - [MODULE 12] - Make it a green stack
    Something wrong with red

--- roots/azure/useast/terraform/locals.tf
+++ roots/azure/useast/terraform/locals.tf
@@ -2,7 +2,7 @@ locals {
   project     = "week-12-stack"
   owner       = "Vihar Chokshi"
   email       = "vc@iot4.net"
-  stack-color = "green"
+  stack-color = "red"
 
   common_tags = {
     Name  = local.project
