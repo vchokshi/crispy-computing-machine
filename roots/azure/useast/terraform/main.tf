3dc568e3d17dd4cfc0ef58d93b6f47fec901109c
Vihar Chokshi <vc@iot4.net>
2022-02-16 17:18:56 -0700

    [AZURE] Cleaned up main with no elk stuff for now

--- roots/azure/useast/terraform/main.tf
+++ roots/azure/useast/terraform/main.tf
@@ -13,38 +13,38 @@ resource "azurerm_virtual_network" "vn" {
   tags                = local.common_tags
 }
 
-resource "azurerm_virtual_network" "elk-vn" {
-  name                = "${local.stack-color}-elk-net"
-  address_space       = ["10.2.0.0/16"]
-  location            = "westus"
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = local.common_tags
-}
+#resource "azurerm_virtual_network" "elk-vn" {
+#name                = "${local.stack-color}-elk-net"
+#address_space       = ["10.2.0.0/16"]
+#location            = "westus"
+#resource_group_name = azurerm_resource_group.rg.name
+#tags                = local.common_tags
+#}
 
-resource "azurerm_virtual_network_peering" "elk-to-vn" {
-  name                      = "${local.stack-color}-net-to-elk"
-  resource_group_name       = azurerm_resource_group.rg.name
-  virtual_network_name      = azurerm_virtual_network.vn.name
-  remote_virtual_network_id = azurerm_virtual_network.elk-vn.id
-}
-resource "azurerm_virtual_network_peering" "vn-to-elk" {
-  name                      = "elk-to-${local.stack-color}-net"
-  resource_group_name       = azurerm_resource_group.rg.name
-  virtual_network_name      = azurerm_virtual_network.elk-vn.name
-  remote_virtual_network_id = azurerm_virtual_network.vn.id
-}
+#resource "azurerm_virtual_network_peering" "elk-to-vn" {
+#name                      = "${local.stack-color}-net-to-elk"
+#resource_group_name       = azurerm_resource_group.rg.name
+#virtual_network_name      = azurerm_virtual_network.vn.name
+#remote_virtual_network_id = azurerm_virtual_network.elk-vn.id
+#}
+#resource "azurerm_virtual_network_peering" "vn-to-elk" {
+#name                      = "elk-to-${local.stack-color}-net"
+#resource_group_name       = azurerm_resource_group.rg.name
+#virtual_network_name      = azurerm_virtual_network.elk-vn.name
+#remote_virtual_network_id = azurerm_virtual_network.vn.id
+#}
 resource "azurerm_subnet" "s" {
   name                 = "${local.stack-color}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vn.name
   address_prefixes     = ["10.0.0.0/24"]
 }
-resource "azurerm_subnet" "e" {
-  name                 = "${local.stack-color}-elk-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.elk-vn.name
-  address_prefixes     = ["10.2.0.0/24"]
-}
+#resource "azurerm_subnet" "e" {
+#name                 = "${local.stack-color}-elk-subnet"
+#resource_group_name  = azurerm_resource_group.rg.name
+#virtual_network_name = azurerm_virtual_network.elk-vn.name
+#address_prefixes     = ["10.2.0.0/24"]
+#}
 
 
 resource "azurerm_network_security_group" "nsg" {
@@ -85,19 +85,19 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = chomp(data.http.myip.body)
     destination_address_prefix = "AzureLoadBalancer"
   }
-  security_rule {
-    name                   = "HTTPKIB"
-    priority               = 1004
-    direction              = "Inbound"
-    access                 = "Allow"
-    protocol               = "Tcp"
-    source_port_range      = "*"
-    destination_port_range = "5601"
-    #source_address_prefix      = chomp(data.http.myip.body)
-    source_address_prefix      = "0.0.0.0"
-    destination_address_prefix = "*"
-  }
-  tags = local.common_tags
+  #security_rule {
+  #name                   = "HTTPKIB"
+  #priority               = 1004
+  #direction              = "Inbound"
+  #access                 = "Allow"
+  #protocol               = "Tcp"
+  #source_port_range      = "*"
+  #destination_port_range = "5601"
+  ##source_address_prefix      = chomp(data.http.myip.body)
+  #source_address_prefix      = "0.0.0.0"
+  #destination_address_prefix = "*"
+  #}
+  #tags = local.common_tags
 }
 
 resource "azurerm_availability_set" "azaz" {
