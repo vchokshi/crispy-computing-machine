0b4b805159b289534a6278c7e125601ef1142262
Vihar Chokshi <vc@iot4.net>
2022-03-01 17:08:36 -0700

    [REIBLOCK] Added REIBLOCK to the mix

--- roots/aws/master/global/organization.tf
+++ roots/aws/master/global/organization.tf
@@ -53,6 +53,37 @@ data "aws_iam_policy_document" "policy" {
   }
 }
 
+resource "aws_organizations_policy" "restricted_policy" {
+  name        = "restricted_policy"
+  description = "AWS Restricted Service Control Policy"
+  type        = "SERVICE_CONTROL_POLICY"
+  content     = data.aws_iam_policy_document.restricted_policy.json
+}
+
+
+data "aws_iam_policy_document" "restricted_policy" {
+  version = "2012-10-17"
+  statement {
+    effect = "Deny"
+    resources = [
+      "*"
+    ]
+    actions = [
+      "ec2:*",
+      "s3:*",
+      "cloudwatch:*",
+      "cloudtrail:*"
+    ]
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:RequestedRegion"
+      values = [
+        "us-east-1"
+      ]
+    }
+  }
+}
+
 resource "aws_organizations_organizational_unit" "main" {
   name      = "iot4"
   parent_id = aws_organizations_organization.org.roots[0].id
@@ -122,3 +153,22 @@ resource "aws_organizations_account" "monitron" {
   email     = "monitron@iot4.net"
   parent_id = aws_organizations_organizational_unit.main.id
 }
+
+
+resource "aws_organizations_organizational_unit" "restricted" {
+  name      = "restricted"
+  parent_id = aws_organizations_organizational_unit.main.id
+}
+
+resource "aws_organizations_policy_attachment" "restricted_attachment" {
+  policy_id = aws_organizations_policy.restricted_policy.id
+  target_id = aws_organizations_organizational_unit.restricted.id
+}
+resource "aws_organizations_account" "reiblock" {
+  name      = "reiblock"
+  email     = "reiblock@iot4.net"
+  parent_id = aws_organizations_organizational_unit.restricted.id
+}
+
+
+
