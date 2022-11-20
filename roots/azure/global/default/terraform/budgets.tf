ea41ffc82b8b13c1c5bfbb4fd2eedabaabc0c2af
VIHAR CHOKSHI <vc@iot4.net>
2022-11-19 20:59:57 -0700

    [VUMC] Added Azure CAF model for VUMC

---
+++ roots/azure/global/default/terraform/budgets.tf
@@ -0,0 +1,63 @@
+resource "azurerm_consumption_budget_subscription" "asu" {
+  name            = "Consumption Budget"
+  subscription_id = "/subscriptions/52c3bf69-8878-40ca-9e80-7358298b2f30"
+
+  amount     = 10
+  time_grain = "Monthly"
+
+  time_period {
+    start_date = "2022-12-01T00:00:00Z"
+  }
+  notification {
+    enabled        = false
+    threshold      = 100.0
+    operator       = "GreaterThan"
+    threshold_type = "Forecasted"
+
+    contact_emails = [
+      "vc@iot4.net"
+    ]
+  }
+}
+resource "azurerm_consumption_budget_subscription" "conn" {
+  name            = "Consumption Budget"
+  subscription_id = "/subscriptions/0b008a7a-8d40-4b4e-a4bd-23cc5c8e2a9a"
+
+  amount     = 10
+  time_grain = "Monthly"
+
+  time_period {
+    start_date = "2022-12-01T00:00:00Z"
+  }
+  notification {
+    enabled        = false
+    threshold      = 90.0
+    operator       = "GreaterThan"
+    threshold_type = "Forecasted"
+
+    contact_emails = [
+      "vc@iot4.net"
+    ]
+  }
+}
+resource "azurerm_consumption_budget_subscription" "iot" {
+  name            = "Consumption Budget"
+  subscription_id = "/subscriptions/91b21a5a-e370-45ba-9bf5-5ac16e47c937"
+  amount          = 10
+  time_grain      = "Monthly"
+
+  time_period {
+    start_date = "2022-12-01T00:00:00Z"
+  }
+  notification {
+    enabled        = false
+    threshold      = 80.0
+    operator       = "GreaterThan"
+    threshold_type = "Forecasted"
+
+    contact_emails = [
+      "vc@iot4.net"
+    ]
+  }
+}
+
