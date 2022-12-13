f251c4a7c0f82da00be97a9cc2fcbc64597a6478
VIHAR CHOKSHI <vc@iot4.net>
2022-12-13 10:53:57 -0700

    [OPENAI] - Adding in some final good code for testing

--- openai/babbage.py
+++ openai/babbage.py
@@ -7,14 +7,19 @@ pp = pprint.PrettyPrinter(indent=1)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+print(f'Type in any incomplete sentence and let me finish it for you!')
+sentence = input(">")
+
 response = openai.Completion.create(
-  model="text-babbage-001",
-    prompt="Finish this sentence.\n\nVertical farming provides a novel solution for producing food locally, reducing transportation costs",
-      temperature=0.29,
-        max_tokens=64,
-          top_p=1,
-            frequency_penalty=0,
-              presence_penalty=0,
-                stop=["."]
-                )
-pp.pprint(response)
+  model="text-ada-001",
+  prompt=f'Finish this sentence.\n\n{sentence}',
+  temperature=0.79,
+  max_tokens=128,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop=["."]
+)
+#pp.pprint(response)
+finish = response.choices[0].text
+print(f'{sentence} {finish}.')
