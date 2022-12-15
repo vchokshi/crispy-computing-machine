b5444af7432edb7a52a790330794f7e9d97fea90
VIHAR CHOKSHI <vc@iot4.net>
2022-12-14 17:34:08 -0700

    [IOT4] Cleaned up us-west-2 AWS

--- roots/aws/master/us-west-2/terraform/backend.tf
+++ roots/aws/master/us-west-2/terraform/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "iot4-tf-state"
-    key            = "terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = "true"
-    dynamodb_table = "iot4-terraform-lock"
+    bucket  = "iot4-tfstate"
+    key     = "roots/aws/us-west-2/iot4.tfstate"
+    region  = "us-east-1"
+    encrypt = "true"
   }
 }
 
--- roots/aws/master/us-west-2/terraform/s3.tf
+++ roots/aws/master/us-west-2/terraform/s3.tf
@@ -1,30 +1,51 @@
-resource "aws_s3_bucket" "terraform" {
-  bucket = "iot4-tf-state"
-  acl    = "private"
-  tags = {
-    Name = var.project_name
-  }
-}
-
-resource "aws_s3_bucket" "lambdas" {
-  bucket = "iot4-lambda-staging"
-  acl    = "private"
-  tags = {
-    Name = var.project_name
-  }
-}
-
-resource "aws_s3_bucket" "iot4_group" {
-  bucket        = "iot4-group"
-  acl           = "public-read"
-  policy        = file("${path.module}/policy.json")
+resource "aws_s3_bucket" "iot4_www" {
+  bucket        = "iot4-www"
   force_destroy = false
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-  tags = {
-    Name = var.project_name
-  }
 }
 
+resource "aws_s3_bucket_policy" "www" {
+  bucket = aws_s3_bucket.iot4_www.id
+  policy = file("${path.module}/policy.json")
+}
+
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.iot4_www.id
+  acl    = "public-read"
+
+}
+resource "aws_s3_bucket_website_configuration" "www" {
+  bucket = aws_s3_bucket.iot4_www.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  routing_rule {
+    condition {
+      key_prefix_equals = "docs/"
+    }
+    redirect {
+      replace_key_prefix_with = "documents/"
+    }
+
+  }
+
+}
+
+resource "aws_route53_record" "www-a" {
+  zone_id = data.aws_route53_zone.public.zone_id
+  name    = "www.${var.domain}"
+  type    = "A"
+  alias {
+    name                   = aws_s3_bucket.iot4_www.website_endpoint
+    zone_id                = aws_s3_bucket.iot4_www.hosted_zone_id
+    evaluate_target_health = true
+
+  }
+
+}
