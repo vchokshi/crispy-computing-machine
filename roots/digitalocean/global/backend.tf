78418b5711683d1d016e504dedee0ee265430164
VIHAR CHOKSHI <vc@iot4.net>
2022-12-24 11:41:53 -0700

    Adds DNS records for K8S nodes

--- roots/digitalocean/global/backend.tf
+++ roots/digitalocean/global/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "iot4-tfstate"
-    key     = "roots/digitalocean/panoptica/do.tfstate"
+    key     = "roots/digitalocean/global/global.tfstate"
     region  = "us-east-1"
     encrypt = "true"
   }
