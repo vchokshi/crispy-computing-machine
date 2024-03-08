a722cb85bf4e804e5895b2b2a896815f41a53ea5
VIHAR CHOKSHI <vc@iot4.net>
2024-03-08 15:57:16 -0700

    Added more hosts to the mix
    Signed-off-by: VIHAR CHOKSHI <vc@iot4.net>

--- roots/aws/finance/eu-west-1/websites/README.md
+++
@@ -1,5 +0,0 @@
-# About
-
-Finance in eu-west uses a website purchased from theme forest called leza
-
-The website is stored here.
--- roots/digitalocean/panoptica/main.tf
+++ roots/digitalocean/panoptica/main.tf
@@ -20,7 +20,7 @@ resource "digitalocean_record" "c" {
 
 variable "instance_count" {
   type    = number
-  default = 3
+  default = 5
 }
 
 variable "instance_size" {
