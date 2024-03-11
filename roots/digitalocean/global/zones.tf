d6c5834aaf881d5a35328912981d0dd5d62398b8
VIHAR CHOKSHI <vc@iot4.net>
2024-03-11 09:51:21 -0700

    Delegate more DNS Zones to Digital Ocean
    Signed-off-by: VIHAR CHOKSHI <vc@iot4.net>

--- roots/digitalocean/global/zones.tf
+++ roots/digitalocean/global/zones.tf
@@ -1,3 +1,11 @@
 resource "digitalocean_domain" "default" {
   name = "do.iot4.net"
 }
+
+resource "digitalocean_domain" "viharc" {
+  name = "do.viharc.com"
+}
+
+resource "digitalocean_domain" "vchokshi" {
+  name = "do.vchokshi.com"
+}
