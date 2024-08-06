2348423638f58b969adceaf896f04c16a67c0bc1
Vihar Chokshi <vchokshi23@gmail.com>
2024-08-05 18:39:25 -0700

    Got CAF working again with SHI

---
+++ roots/azure/global/default/terraform/caf.tf
@@ -0,0 +1,34 @@
+variable "root_id" {
+  type    = string
+  default = "SHI"
+}
+
+variable "root_name" {
+  type    = string
+  default = "Stratascale"
+}
+
+variable "default_location" {
+  type    = string
+  default = "eastus"
+}
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "6.0.0"
+  #version = "2.4.0"
+
+  deploy_demo_landing_zones = false
+  disable_telemetry         = true
+  default_location          = var.default_location
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+  root_id        = var.root_id
+  root_name      = var.root_name
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+  deploy_connectivity_resources = false
+}
