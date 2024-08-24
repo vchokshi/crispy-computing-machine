3a6cefe1f063e293b756a5e871bdb04e430dd5ad
Vihar Chokshi <vchokshi23@gmail.com>
2024-08-24 01:50:25 -0700

    Just moving subsriptions around

--- roots/azure/global/default/terraform/data.tf
+++ roots/azure/global/default/terraform/data.tf
@@ -8,3 +8,14 @@ output "management_subscription" {
 }
 data "azuread_domains" "all" {
 }
+
+data "azurerm_billing_mpa_account_scope" "vciot" {
+  billing_account_name = "58b9db98-4018-5bff-d0c9-5ef373d30095:f2268dfb-6408-44f8-8e4f-fb07d1857c66_2019-05-31"
+  customer_name        = "F56B-CESX-BG7-PGB"
+
+}
+
+output "id" {
+  value = data.azurerm_billing_mpa_account_scope.vciot.id
+
+}
--- roots/azure/global/default/terraform/subscriptions.tf
+++ roots/azure/global/default/terraform/subscriptions.tf
@@ -4,6 +4,6 @@ data "azurerm_subscriptions" "available" {}
 
 resource "azurerm_subscription" "mfs" {
   alias             = "my_first_subscription"
-  subscription_name = "IOT4 Subscription"
+  subscription_name = "Subscription"
   subscription_id   = data.azurerm_subscription.current.subscription_id
 }
